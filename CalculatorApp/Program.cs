f05baf1254c38bafb10781c53ce52929b7ba405f
José González <jgonzalez.242720@gmail.com>
2025-08-11 20:26:27 -0600

    feat: agregar proyecto de calculadora en consola en .NET

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,34 @@
+﻿//
+Console.WriteLine("Ingrese el primer número:");
+int number1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Ingrese la operación (+, -, *, /):");
+string operation = Console.ReadLine() ?? string.Empty;
+
+Console.WriteLine("Ingrese el segundo número:");
+int number2 = int.Parse(Console.ReadLine() ?? "0");
+
+switch(operation)
+{
+    case "+":
+        Console.WriteLine("El resultado es: " + (number1 + number2));
+        break;
+    case "-":
+        Console.WriteLine("El resultado es: " + (number1 - number2));
+        break;
+    case "*":
+        Console.WriteLine("El resultado es: " + (number1 * number2));
+        break;
+    case "/":
+        if (number2 != 0)
+        {
+            Console.WriteLine("El resultado es: " + ((double)number1 / number2));
+        }
+        else if (number2 == 0) {
+            Console.WriteLine("Error: División por cero no es permitida.");
+        }
+        else {
+            Console.WriteLine("Indefinido");
+        }
+        break;
+}
